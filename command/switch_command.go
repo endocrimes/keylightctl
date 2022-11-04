a130922909753939e7476539e0a84b28cddd4015
Philip Deuchler <pbdeuchler@gmail.com>
2022-11-04 16:10:49 -0600

    Add toggle command

--- command/switch_command.go
+++ command/switch_command.go
@@ -86,8 +86,8 @@ func (c *SwitchCommand) Run(args []string) int {
 		return 1
 	}
 
-	if args[0] != "on" && args[0] != "off" {
-		c.UI.Error("Argument must be 'on' or off'")
+	if args[0] != "on" && args[0] != "off" && args[0] != "toggle" {
+		c.UI.Error("Argument must be 'on', 'off', or 'toggle'")
 		c.UI.Error(commandErrorText(c))
 		return 1
 	}
@@ -95,6 +95,16 @@ func (c *SwitchCommand) Run(args []string) int {
 	desiredPowerState := 0
 	if args[0] == "on" {
 		desiredPowerState = 1
+	} else if args[0] == "toggle" {
+		desiredPowerState = -1 // a bit hacky, but allows for minimal changes
+	}
+
+	if brightness >= 0 || temperature >= 0 {
+		if desiredPowerState == -1 {
+			c.UI.Error("Cannot specify brightness and temperature while toggling light(s)")
+			c.UI.Error(commandErrorText(c))
+			return 1
+		}
 	}
 
 	if allLights && len(requestedLights) != 0 {
@@ -134,7 +144,15 @@ func (c *SwitchCommand) Run(args []string) int {
 
 		newOpts := opts.Copy()
 		for _, l := range newOpts.Lights {
-			l.On = desiredPowerState
+			if desiredPowerState == -1 {
+				if l.On == 0 {
+					l.On = 1
+				} else {
+					l.On = 0
+				}
+			} else {
+				l.On = desiredPowerState
+			}
 			if temperature >= 0 {
 				l.Temperature = temperature
 			}
